bf34cac173e6c63d3c7bc9af14f0ecb68020a93a
Denis <ar.denis.by@gmail.com>
2026-05-05 21:42:22 +0300

    feat(di): add wallet uow and service providers

--- app/di/container.py
+++ app/di/container.py
@@ -7,6 +7,9 @@ from app.database.config import settings
 from app.modules.users.service.use_cases import CreateUserService, LoginUserService, UpdateUserService, \
     DeleteUserService, ShowUserService
 from app.modules.users.uow.uow import UserUnitOfWork
+from app.modules.wallets.service.use_cases import CreateWalletService, UpdateWalletService, DeleteWalletService, \
+    ManageWalletService, ShowWalletService
+from app.modules.wallets.uow.uow import WalletUnitOfWork
 
 
 class AsyncEngineProvider(Provider):
@@ -56,6 +59,11 @@ class UnitOfWorkProvider(Provider):
         async with UserUnitOfWork(async_session) as user_uow:
             yield user_uow
 
+    @provide
+    async def wallet_uow(self, async_session: AsyncSession) -> AsyncGenerator[WalletUnitOfWork, None]:
+        async with WalletUnitOfWork(async_session) as wallet_uow:
+            yield wallet_uow
+
 
 class UserServiceProvider(Provider):
     """Провайдер для создания сервисов пользователя"""
@@ -83,13 +91,39 @@ class UserServiceProvider(Provider):
         return ShowUserService(user_uow)
 
 
+class WalletServiceProvider(Provider):
+
+    scope = Scope.REQUEST
+
+    @provide
+    def create_service(self, wallet_uow: WalletUnitOfWork) -> CreateWalletService:
+        return CreateWalletService(wallet_uow)
+
+    @provide
+    def update_service(self, wallet_uow: WalletUnitOfWork) -> UpdateWalletService:
+        return UpdateWalletService(wallet_uow)
+
+    @provide
+    def delete_service(self, wallet_uow: WalletUnitOfWork) -> DeleteWalletService:
+        return DeleteWalletService(wallet_uow)
+
+    @provide
+    def manage_service(self, wallet_uow: WalletUnitOfWork) -> ManageWalletService:
+        return ManageWalletService(wallet_uow)
+
+    @provide
+    def show_service(self, wallet_uow: WalletUnitOfWork) -> ShowWalletService:
+        return ShowWalletService(wallet_uow)
+
+
 def build_async_container() -> AsyncContainer:
     return make_async_container(
         AsyncEngineProvider(),
         AsyncSessionMakerProvider(),
         AsyncSessionProvider(),
         UnitOfWorkProvider(),
-        UserServiceProvider()
+        UserServiceProvider(),
+        WalletServiceProvider(),
     )
 
 
